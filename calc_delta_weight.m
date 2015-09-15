7705560f5db4884fee5e6fb701e283373ec28230
Tim Hosman <timhosman@hotmail.com>
2015-09-15 10:12:46 +0200

    dweight voor de laatste layer werkt :)

--- calc_delta_weight.m
+++ calc_delta_weight.m
@@ -2,7 +2,7 @@ function dweight = calc_delta_weight(  input_outputlayer, learning_rate, gradien
 % CalcDeltaw Berekend de delta w
 % input_outputlayer is de input op de laatste (en dus output) layer
 
-    dweight = learning_rate .* input_outputlayer * gradient;
+    dweight = learning_rate * gradient' * input_outputlayer;
 
 end
 
