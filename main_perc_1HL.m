ba7d3c4a08cbe2cc2e1f650de6cdfa97f9cdf3a9
Tim Hosman <timhosman@hotmail.com>
2015-09-16 16:18:51 +0200

    1HL en 3HL geupdate

--- main_perc_1HL.m
+++ main_perc_1HL.m
@@ -2,30 +2,43 @@ clear all;
 close all;
 
 %% Tweakables
-epoch = 30;     % Number of epochs
-thres = 2;      % Threshold
-alpha = 0.1;    % Learning rate
-%len_L1 = 8;     % Number of nodes in layer 1
-len_L2 = 6;     % Number of nodes in layer 2
-len_L3 = 3;     % Number of nodes in layer 3
+epoch = 10;         % Number of epochs
+thres = 2;          % Threshold
+alpha = 0.05;       % Learning rate
+beta = 0.9;         % Momentum constant
+alpha_inc = 1.1;    % alpha increase rate
+alpha_dec = 0.5;    % alpha decrease rate
+alpha_max = 0.95;   % max alpha value
+%len_L1 = 8;         % Number of nodes in layer 1
+len_L2 = 6;         % Number of nodes in layer 2
+len_L3 = 3;         % Number of nodes in layer 3
+K = 8;              % cross validation constant (DO NOT CHANGE)
+
+%% Initializing some constants
 mse = zeros(1,epoch);
 mse_sum = 0;
+dw_L3 = 0;
+dw_L2 = 0;
+%dw_L1 = 0;
+alpha_hist = zeros(1,epoch);
+test_factor = (K-2)/K;
 
 %% Fetch input matrix and desired output
 x_in = dlmread('features.txt');
-y_out_desired_cat = dlmread('targets.txt');
-y_out_desired = de2bi(y_out_desired_cat);
-
-% Calculate amount of data (len_in) and amount of inputs (width_in)
 [len_in,width_in] = size(x_in);
+len_in = round(len_in*test_factor);
+x_in = x_in(1:len_in,:); %take training set of data
+y_out_desired_cat = dlmread('targets.txt');
+y_out_desired_cat = y_out_desired_cat(1:len_in,:); %take training set of data
+y_out_desired = de2bi(y_out_desired_cat);
 
 %% Initialize weights (rows are different nodes of a layer, columns are ...
 % different weights of inputs of that node)
 % randi(a,b,c) makes a matrix of b x c and fills it with numbers between...
 % 1 and 1000. We normalize to have the numbers range from 0.001 to 1.
-%w_L1 = randi(1000, len_L1, width_in)./1000;
-w_L2 = randi(1000, len_L2, width_in)./1000;
-w_L3 = randi(1000, len_L3, len_L2)./1000;
+%w_L1 = randi(100000, len_L1, width_in)./100000;
+w_L2 = randi(100000, len_L2, width_in)./100000;
+w_L3 = randi(100000, len_L3, len_L2)./100000;
 
 %% Initialize outputs
 %y_L1 = zeros(len_in,len_L1);
@@ -63,9 +76,15 @@ for (m=1:len_in)
     end
     
 %% Calculate new weights
+dw_L3_prev = dw_L3;
+dw_L2_prev = dw_L2;
+% dw_L1_prev = dw_L1;
 [dw_L3,gradient_L3,error_L3] = give_deltaw(y_out_desired(m,:),y_L3(m,:),y_L3(m,:),y_L2(m,:),alpha);
 [dw_L2,gradient_L2] = give_deltaw_hidden(y_L2(m,:),gradient_L3,w_L3,x_in(m,:),alpha);
 %[dw_L1,gradient_L1] = give_deltaw_hidden(y_L1(m,:),gradient_L2,w_L2,x_in(m,:),alpha);
+dw_L3 = beta * dw_L3_prev + dw_L3;
+dw_L2 = beta * dw_L2_prev + dw_L2;
+%dw_L1 = beta * dw_L1_prev + dw_L1;
 w_L3 = w_L3 + dw_L3;
 w_L2 = w_L2 + dw_L2;
 %w_L1 = w_L1 + dw_L1;
@@ -75,7 +94,22 @@ mse_sum = (mse_sum + sum(error_L3.^2));
 end
 mse(x) = mse_sum/len_in;
 mse_sum = 0;
+
+%% Calculate error rate
+y_L3_cat = bin2dec(y_L3_bin);
+error_rate(x) = sum(y_L3_cat ~= y_out_desired_cat)/len_in*100;
+
+%% Calculate adaptive learning rate
+alpha_hist(x) = alpha;
+if (x>1)                            %if not first epoch
+    if (alpha>alpha_max)
+        alpha = alpha_max;
+    elseif ((mse(x-1)-mse(x))>0)    %error decreasing
+        alpha = alpha_inc * alpha;
+    else                            %error increasing
+        alpha = alpha_dec * alpha;
+    end
+end
 end
 
-%y_L3_cat = bi2dec(num2str(y_L3_bin));
-
+y_L3_cat = bin2dec(y_L3_bin);
