d0e05997b583afaf29ce648d9d709232110f6bc9
Tim Hosman <timhosman@hotmail.com>
2015-09-17 23:34:51 +0200

    main_trained uitgebreid

--- main_trained_HL3.m
+++ main_trained_HL3.m
@@ -1,4 +1,4 @@
-clearvars -except w_L1 w_L2 w_L3 thres len_L1 len_L2 len_L3;
+clearvars -except w_L1 w_L2 w_L3 thres len_L0 len_L1 len_L2 len_L3;
 close all;
 
 %% Fetch input matrix and desired output
@@ -18,11 +18,19 @@ y_L3 = zeros(len_in,len_L3);
 y_L3_bin = zeros(len_in,len_L3);
 
 for (m = 1:len_in)
+    %% Generate outputs zero-th layer
+    for (n=1:len_L0)
+        %x_L1(n,m), where n selects the node of L1, and m selects which ...
+        % data input is used (1-7854)
+        y_L0(m,n) = perc(x_in(m,:), w_L0(n,:), thres);
+        n = n+1;
+    end
+    
     %% Generate outputs first layer
     for (n=1:len_L1)
         %x_L1(n,m), where n selects the node of L1, and m selects which ...
         % data input is used (1-7854)
-        y_L1(m,n) = perc(x_in(m,:), w_L1(n,:), thres);
+        y_L1(m,n) = perc(y_L0(m,:), w_L1(n,:), thres);
         n = n+1;
     end
 
