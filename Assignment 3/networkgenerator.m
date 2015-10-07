5fb437aeeb8ad52bd8cbfca91adecf8eda13faa3
unknown <bugattid@gmail.com>
2015-10-07 16:57:15 +0200

    Coordinaten en maze worden correct uitgelezen

--- Assignment 3/networkgenerator.m
+++ Assignment 3/networkgenerator.m
@@ -1,6 +1,20 @@
 maze = dlmread('easy maze.txt');
-test = dlmread('easy coordinates.txt','; ');
-coordinates = textscan('easy coordinates.txt','%s');
+%coordinates are in [x,y] position
+startcoord = dlmread('easy coordinates.txt',',', [0 0 0 1]); 
+endcoord = dlmread('easy coordinates.txt',',', [1 0 1 1]);
+
 sizeX = maze(1,1); %get number of columns
 sizeY = maze(1,2); %get number of rows
+size = [sizeY sizeX]; %reverse sizeX and sizeY order for easy calculating of starting position
 maze = maze(2:end, 1:end); %remove top line which only contains size data
+
+%convert coordinates to matrix positions
+startposcol = startcoord(1);
+startposrow = sizeY - startcoord(2);
+endposcol = endcoord(1);
+endposrow = sizeY - endcoord(2);
+
+%combine start and end positions in startpos and endpos variables
+startpos = [startposrow startposcol];
+endpos = [endposrow endposcol];
+
