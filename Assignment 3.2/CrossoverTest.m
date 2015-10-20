75bba9cf2e4eb947feff03725d4e3341f7adddc7
unknown <bugattid@gmail.com>
2015-10-20 18:05:21 +0200

    Eerste versie van crossover functie. Het lijkt goed te gaan behalve als een getal voor beide parents in de crossover range zit

---
+++ Assignment 3.2/CrossoverTest.m
@@ -0,0 +1,6 @@
+parent1 = [1 2 3 4 5 6 7 8];
+parent2 = [5 7 8 6 2 4 1 3];
+
+[child1 child2] = DoCrossover(parent1,parent2);
+child1
+child2
--- Assignment 3.2/DoCrossover.m
+++ Assignment 3.2/DoCrossover.m
@@ -2,8 +2,10 @@ function [ child1, child2 ] = DoCrossover( parent1, parent2)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
     
-    length = randi(6);
-    start = randi(18 - length);
+    %length = randi(6);
+    length = 3;
+    %start = randi(8 - length);
+    start = 5;
     part1 = parent1(start:start+length);
     part2 = parent2(start:start+length);
     child1 = parent1;
@@ -13,8 +15,23 @@ function [ child1, child2 ] = DoCrossover( parent1, parent2)
         child2(i) = parent1(i);
     end
     for k = 1:length
-        a = part1(k);
-        b = find(parent1 == a);
+        k
+      index1 = find(part2(k) == parent1)  %vind waar het getal in parent1 staat dat in part2 staat
+      index2 = find(part1(k) == parent2) %vind waar het getal in parent2 staat dat in part1 staat
+      %if((index1 >= start) && index1 <= start+length) %hetzelfde getal zit in beide crossover gebieden
+        
+          %do nothing
+      %else
+          temp = child1(index1); %swap
+          child1(index1) = child2(index2);
+          child2(index2) = temp;
+      %end
+      
+      
+%         a = part1(k); %op plek b moet part2(k) komen
+%         b = find(parent1 == a);
+%         child1(b) = part2(k);
+        
     end
 end
 
